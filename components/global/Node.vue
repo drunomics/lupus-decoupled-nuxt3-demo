0e7ae68aa73a535721c7f17cd382acc88c9018ce
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2023-07-20 14:40:36 +0200

    fix body field

--- components/global/Node.vue
+++ components/global/Node.vue
@@ -2,7 +2,7 @@
   <div class="node">
     <h2>Title: {{ title }}</h2>
     <div v-if="image" v-html="image.content" />
-    <div v-if="body" v-html="body.content" />
+    <div v-if="body" v-html="body" />
     <div v-if="sections">
       <component :is="useDrupalCe().renderCustomElements(sections)" />
     </div>
