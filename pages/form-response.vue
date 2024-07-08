2da7ef47e4ad16a497b90fa85c261255ac6765f1
Alexandru <videoloss3@gmail.com>
2024-07-08 22:34:58 +0300

    LDP-2492: Add messages and use page middleware

--- pages/form-response.vue
+++ pages/form-response.vue
@@ -3,7 +3,11 @@
 </template>
 
 <script setup>
-const { getPage, renderCustomElements } = useDrupalCe()
+const { getPage, renderCustomElements, getMessages } = useDrupalCe()
+
+definePageMeta({
+  middleware: 'form-handler',
+})
 
 const response = useState('form-response', () => null)
 
@@ -14,9 +18,18 @@ if (import.meta.server) {
 
 if (response.value) {
   const isRedirect = response.value.redirect
+  const messages = response.value.messages
   if (isRedirect) {
-    await navigateTo(isRedirect.url, {
-      external: isRedirect.external,
+    onMounted(async () => {
+      if (messages) {
+        getMessages().value.push({
+          type: messages.error ? 'error' : 'success',
+          message: messages.error || messages.success,
+        })
+      }
+      await navigateTo(isRedirect.url, {
+        external: isRedirect.external,
+      })
     })
   } else {
     getPage().value = response.value
