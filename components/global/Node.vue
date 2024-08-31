0d9d8ca566f91be59800dbaae14430d01392155f
harrypango <harry.pango@gmail.com>
2024-08-31 13:24:59 +0200

    added sections prop and div component in Node.vue

--- components/global/Node.vue
+++ components/global/Node.vue
@@ -3,7 +3,9 @@
     <h2>Node: {{ title }}</h2>
     <div v-if="image" v-html="image.content" />
     <div v-if="body" v-html="body" />
-    <div v-if="sections" />
+    <div v-if="sections">
+      <component :is="useDrupalCe().renderCustomElements(sections)" />
+    </div>
   </div>
 </template>
 
@@ -14,5 +16,6 @@ const props = defineProps<{
   created: Number;
   body?: String[];
   image?: Object;
+  sections?: Object;
 }>()
 </script>
