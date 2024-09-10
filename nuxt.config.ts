d737d14471b37c84179e055bdea0ab307def964b
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2024-09-10 11:15:02 -0700

    chore: update to nuxtjs-drupal-ce 2.1.0 and latest nuxt release

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['nuxtjs-drupal-ce', "@nuxt/eslint"],
+
   drupalCe: {
     drupalBaseUrl: 'http://lupus-decoupled.ddev.site',
     exposeAPIRouteRules: true,
   },
+
   nitro: {
     compressPublicAssets: true,
   },
+
+  compatibilityDate: '2024-09-10',
 })
