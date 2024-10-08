3dcd89429c338a8895128bbfc2ff5bd723e2ca76
Harry <harry.pango@gmail.com>
2024-10-08 23:31:45 +0200

    Make use of useCeApi function, changed to client-only component & cleaned up code

--- components/Navigation/NavigationLogin.vue
+++ components/Navigation/NavigationLogin.vue
@@ -1,28 +1,34 @@
 <template>
-  <ul style="list-style: none;">
-    <li v-if="loginStatus">
-      <nuxt-link to="/user/logout">Logout</nuxt-link>
-    </li>
-    <li v-else>
-      <nuxt-link to="/user/login?destination=/">Login</nuxt-link>
-    </li>
-  </ul>
+  <ClientOnly>
+    <ul style="list-style: none;">
+      <li v-if="loginStatus">
+        <nuxt-link to="/user/logout">Logout</nuxt-link>
+      </li>
+      <li v-else>
+        <nuxt-link to="/user/login?destination=/">Login</nuxt-link>
+      </li>
+    </ul>
+  </ClientOnly>
 </template>
 
 <script setup>
+const { useCeApi } = useDrupalCe()
+
 const loginStatus = ref(false)
 
-onMounted(async () => {
-try {
-  const response = await fetch('/api/drupal-ce/user/login_status?_format=json')
-  if (response.ok) {
-    const data = await response.json()
-    loginStatus.value = data.logged_in
-  } else {
-    console.error('Failed to fetch login status')
+const { data: loginData } = await useAsyncData(
+  'loginStatus',
+  () => useCeApi('/user/login_status', {
+    query: {
+      _format: 'json'
+    }
+  }),
+  { server: false }
+)
+
+watchEffect(() => {
+  if (loginData.value) {
+    loginStatus.value = loginData.value.logged_in
   }
-} catch (error) {
-  console.error('Error fetching login status:', error)
-}
 })
 </script>
