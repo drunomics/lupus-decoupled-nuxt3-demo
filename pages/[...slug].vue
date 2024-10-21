f64bac36ee34e0d2da1e73839b0fdbcce15f106b
Harry <harry.pango@gmail.com>
2024-10-21 12:57:57 +0200

    Make code simpler to read

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -12,12 +12,13 @@
 
 <script lang="ts" setup>
 const { fetchPage, renderCustomElements, getPageLayout } = useDrupalCe()
-const page = await fetchPage(useRoute().path, { query: useRoute().query })
-// Set to false to support custom layouts, using <NuxtLayout> instead.
-  definePageMeta({
-  layout: false,
+const route = useRoute()
+const page = await fetchPage(route.path, { query: route.query })
+
+definePageMeta({
+  layout: getPageLayout(page)
 })
-const layout = getPageLayout(page)
+
 useHead({
   title: page.value.title,
   meta: page.value.metatags.meta,
