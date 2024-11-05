d5d82cb4c634cf5329fef6841fb88bf0c697621d
Hari Pango <132347928+harrypango@users.noreply.github.com>
2024-11-05 06:07:30 +0100

    feat: LDP-2606: Add Login/Logout link toggle (#45)
    * feature: LDP-2606: Add Login/Logout link toggle
    
    * Make use of useCeApi function, changed to client-only component & cleaned up code
    
    * clean up code
    
    * clean up code, fix minor issues
    
    * Add new SiteLoginLink component with account menu items
    
    * move to footer and simplify component
    
    * add component
    
    ---------
    
    Co-authored-by: Wolfgang Ziegler <wolfgang.ziegler@drunomics.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,5 +5,14 @@
     <div id="main">
       <slot />
     </div>
+    <footer>
+        <NavigationAccount />
+    </footer>
   </div>
 </template>
+
+<style>
+#main {
+  margin-bottom: 200px;
+}
+</style>
