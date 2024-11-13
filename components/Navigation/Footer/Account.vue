014c08b047934530db5e37c22a38064feb9a3238
Matic Fink <turtlman@gmail.com>
2024-11-13 09:18:26 +0100

    improve: remove hard-coded footer links.

--- components/Navigation/Footer/Account.vue
+++ components/Navigation/Footer/Account.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container flex flex-wrap items-center justify-center gap-2 mt-3">
+  <div class="container flex flex-wrap items-center justify-center gap-2">
     <div v-for="menuItem in accountMenu" :key="menuItem.key">
-      <UiButton variant="secondary" size="xs" as="a" :href="menuItem.relative">
+      <UiButton variant="link" size="xs" as="a" :href="menuItem.relative">
         {{ menuItem.title }}
       </UiButton>
     </div>
