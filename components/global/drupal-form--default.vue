62e3f1afca8bf56a1e3971d4d183428bb371792b
Arthur Lorenz <arthur@utor.at>
2024-12-19 15:15:47 +0100

    feat: Support antibot module.

--- components/global/drupal-form--default.vue
+++ components/global/drupal-form--default.vue
@@ -5,16 +5,60 @@
     v-bind="attributes"
     :action="useRoute().fullPath"
     class="drupal-form"
+    ref="form"
   >
     <slot><div v-html="content" /></slot>
   </form>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   formId: string
   attributes: object
   method: string
   content?: string
 }>()
+
+const form = ref(null)
+
+const unlockForm = () => {
+  const input = form.value?.querySelector('input[name="antibot_key"]');
+  if (input) {
+    input.value = props.attributes.dataAntibotKey.split("").reverse().join("").match(/.{1,2}/g).map((value) => value.split("").reverse().join("")).join("");
+    removeEventListeners();
+  }
+}
+
+const removeEventListeners = () => {
+  form.value?.removeEventListener('mousemove');
+  form.value?.removeEventListener('touchmove');
+  form.value?.removeEventListener('keydown');
+}
+
+if (props.attributes.dataAntibotKey) {
+  onMounted(() => {
+    form.value?.addEventListener('mousemove', function () {
+      // Unlock the forms.
+      unlockForm();
+    });
+
+    // Wait for a touch move event, indicating that they are human.
+    form.value?.addEventListener('touchmove', function () {
+      // Unlock the forms.
+      unlockForm();
+    });
+
+    // A tab or enter key pressed can also indicate they are human.
+    form.value?.addEventListener('keydown', function (e) {
+      if ((e.code == 'Tab') || (e.code == 'Enter')) {
+        // Unlock the forms.
+        unlockForm();
+      }
+    });
+  })
+
+  onUnmounted(() => {
+    removeEventListeners();
+  })
+}
 </script>
