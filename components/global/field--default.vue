d2c8981c571e22e18c35e9708a59d04c89c60cc2
Wolfgang Ziegler <wolfgang.ziegler@drunomics.com>
2025-01-19 04:06:59 +0000

    add field default

---
+++ components/global/field--default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <slot>
+        <component :is="useDrupalCe().renderCustomElements($attrs.content)" />
+      </slot>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  defineSlots<{
+    default();
+  }>()
+  </script>
+  
