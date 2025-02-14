e6d4cec05ba892a9670e5e15c763d122f1fc0a05
Alexandru <videoloss3@gmail.com>
2025-02-14 14:27:42 +0200

    LDP-2726: Add default form styling

--- components/global/drupal-form--default.vue
+++ components/global/drupal-form--default.vue
@@ -22,3 +22,24 @@ defineSlots<{
   default();
 }>()
 </script>
+
+<style>
+.drupal-form {
+  display: flex;
+  flex-direction: column;
+  padding: 0 1rem;
+}
+
+.drupal-form label {
+  display: block;
+  margin-bottom: 0.325rem;
+}
+
+.drupal-form input {
+  padding: 0.5rem;
+}
+
+.drupal-form .form-item {
+  margin-bottom: 0.75rem;
+}
+</style>
