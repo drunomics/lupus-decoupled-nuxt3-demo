00960429d4228d4070fd55c4d4f70b9cd0f19be9
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2025-10-12 11:20:14 +0200

    better node

--- components/global/node--default.vue
+++ components/global/node--default.vue
@@ -1,13 +1,17 @@
 <template>
   <article class="node">
-    <h2 v-if="title" class="text-3xl font-bold tracking-tight mb-6">{{ title }}</h2>
-    <slot name="image">
-      <component :is="useDrupalCe().renderCustomElements(image)" />
-    </slot>
-    <slot name="body">
-      <component :is="useDrupalCe().renderCustomElements(body)" />
-    </slot>
-    <div v-if="sections">
+    <h1 v-if="title" class="text-3xl font-bold tracking-tight mb-6">{{ title }}</h1>
+    <div v-if="image" class="node-image mb-6">
+      <slot name="image">
+        <component :is="useDrupalCe().renderCustomElements(image)" />
+      </slot>
+    </div>
+    <div v-if="body" class="node-body">
+      <slot name="body">
+        <component :is="useDrupalCe().renderCustomElements(body)" />
+      </slot>
+    </div>
+    <div v-if="sections" class="node-sections mt-6">
       <component :is="useDrupalCe().renderCustomElements(sections)" />
     </div>
   </article>
